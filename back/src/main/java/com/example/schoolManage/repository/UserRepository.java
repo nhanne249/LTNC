6b1f295c6c12404b5df1a5180e75725dcdcc4075
Khang <ldkhang1201@gmail.com>
2024-04-02 15:50:38 +0700

    xoa back (#11)

--- back/src/main/java/com/example/schoolManage/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.schoolManage.repository;
-
-import com.example.schoolManage.model.user.User;
-import org.bson.types.ObjectId;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface UserRepository extends MongoRepository<User, ObjectId> {
-}
