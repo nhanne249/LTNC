306c0e2f3b93c2b6ad41615b7b7c501a2e545fdd
blueonline07 <ldkhang1201@gmail.com>
2024-04-09 01:16:23 +0700

    feat: crud users

--- back/src/main/java/com/example/schoolManage/controller/AdminController.java
+++ back/src/main/java/com/example/schoolManage/controller/AdminController.java
@@ -3,6 +3,8 @@ package com.example.schoolManage.controller;
 
 import com.example.schoolManage.model.course.Course;
 import com.example.schoolManage.model.course.Classroom;
+import com.example.schoolManage.model.user.Student;
+import com.example.schoolManage.model.user.Teacher;
 import com.example.schoolManage.model.user.User;
 import com.example.schoolManage.service.AdminService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,6 +17,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Objects;
 
 @RestController
 @RequestMapping
@@ -22,13 +25,27 @@ import java.util.List;
 public class AdminController {
     @Autowired
     private AdminService adminService;
-    @GetMapping("/all")
+    @GetMapping("/all/users")
     public ResponseEntity<List<User>> allUser(){
         return new ResponseEntity<List<User>>(adminService.getAllUsers(), HttpStatus.OK);
     }
     @GetMapping("/{username}")
     public ResponseEntity<User> getUserBy(@PathVariable String username){
         return new ResponseEntity<User>(adminService.getUser(username), HttpStatus.OK);
+    }
+    @PostMapping("/register/student")
+    public ResponseEntity<Student> addStudent(@RequestBody Student student){
+        Student st = adminService.createStudent(student);
+        if(Objects.isNull(st))
+            return ResponseEntity.badRequest().build();
+        return new ResponseEntity<Student>(st, HttpStatus.CREATED);
+    }
+    @PostMapping("/register/teacher")
+    public ResponseEntity<Teacher> addTeacher(@RequestBody Teacher teacher){
+        Teacher tc = adminService.createTeacher(teacher);
+        if(Objects.isNull(tc))
+            return ResponseEntity.badRequest().build();
+        return new ResponseEntity<Teacher>(tc, HttpStatus.CREATED);
     }
     @DeleteMapping("/{username}")
     public ResponseEntity<String> deleteUser(@PathVariable String username){
@@ -48,12 +65,5 @@ public class AdminController {
     @PostMapping("/class")
     public ResponseEntity<Classroom> addClass(@RequestBody Classroom classroom){
         return new ResponseEntity<Classroom>(adminService.addClass(classroom),HttpStatus.CREATED);
-    }
-    public UserDetails getLoggedInUserDetails(){
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        if(authentication != null && authentication.getPrincipal() instanceof UserDetails){
-            return (UserDetails) authentication.getPrincipal();
-        }
-        return null;
     }
 }
--- back/src/main/java/com/example/schoolManage/service/AdminService.java
+++ back/src/main/java/com/example/schoolManage/service/AdminService.java
@@ -2,42 +2,71 @@ package com.example.schoolManage.service;
 
 import com.example.schoolManage.model.course.Course;
 import com.example.schoolManage.model.course.Classroom;
+import com.example.schoolManage.model.user.Student;
+import com.example.schoolManage.model.user.Teacher;
 import com.example.schoolManage.model.user.User;
 import com.example.schoolManage.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.data.mongodb.core.query.UpdateDefinition;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import javax.swing.plaf.ColorUIResource;
 import java.util.List;
 
 @Service
 public class AdminService{
     @Autowired
-    private UserRepository userRepository;
+    private PasswordEncoder passwordEncoder;
     @Autowired
     private MongoTemplate mongoTemplate;
     public List<User> getAllUsers() {
-        return userRepository.findAll();
+        return mongoTemplate.findAll(User.class, "users");
     }
-
+    public Student createStudent(Student student){
+        if(mongoTemplate.exists(Query.query(Criteria.where("username").is(student.getUsername())),"users" )){
+            return null;
+        }
+        return mongoTemplate.insert(new Student(student.getUsername()
+                , passwordEncoder.encode(student.getPassword())
+                , student.getName(), student.getStudentId()
+                , student.getEmail(), student.getPhoneNumber()), "users");
+    }
+    public Teacher createTeacher(Teacher teacher){
+        if(mongoTemplate.exists(Query.query(Criteria.where("username").is(teacher.getUsername())),"users" )){
+            return null;
+        }
+        return mongoTemplate.insert(new Teacher(teacher.getUsername(),
+                passwordEncoder.encode(teacher.getPassword()),
+                teacher.getName(),
+                teacher.getEmail(),
+                teacher.getPhoneNumber()), "users");
+    }
     public User getUser(String username) {
         return mongoTemplate.findOne(Query.query(Criteria.where("username").is(username)), User.class, "users");
     }
-
+//    public User updateUser(String username){
+//
+//    }
     public void deleteUser(String username) {
         //EXCEPTION STUDENT NOT FOUND
         mongoTemplate.findAndRemove(Query.query(Criteria.where("username").is(username)), User.class, "users");
     }
-
-    public User createUser(User user) {
-        return mongoTemplate.insert(user, "users");
-    }
     public Course addCourse(Course course) {
         return mongoTemplate.insert(course, "courses");
     }
-
+    public List<Course> getAllCourses(){
+        return mongoTemplate.findAll(Course.class, "users");
+    }
+    public Course getCourse(String courseId){
+        return mongoTemplate.findOne(Query.query(Criteria.where("courseId").is(courseId)), Course.class, "courses");
+    }
+//    public Course updateCourse(String courseId){
+//
+//    }
     public void deleteCourse(String courseId) {
         mongoTemplate.findAndRemove(Query.query(Criteria.where("courseId").is(courseId)), Course.class, "courses");
     }
