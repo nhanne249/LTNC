757fc15f88167242572f834e48e417b4d5b9ea12
nguyenhung2912200 <nguyenhung202912@gmail.com>
2024-04-15 14:10:30 +0700

    15/4/2024

--- front/src/features/Admin/Class/ClassList/index.jsx
+++ front/src/features/Admin/Class/ClassList/index.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import "./index.scss";
 
-const UserList = () => {
+const ClassList = () => {
     return (<div></div>);
 }
 
-export default UserList;
+export default ClassList;
--- front/src/features/Teacher/PersonalInformation/index.jsx
+++ front/src/features/Teacher/PersonalInformation/index.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import "./index.scss";
 
-const UserList = () => {
+const PersonalInformation = () => {
     return (<div></div>);
 }
 
-export default UserList;
+export default PersonalInformation;
