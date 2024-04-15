46e47ca7baaeed5fc19f7aa95301c1dcd0c6a0c2
Nguyễn Quốc Hùng <81325847+HarryDom2912@users.noreply.github.com>
2024-04-15 14:20:02 +0700

    Quoc hung (#19)
    * 11/4/2024
    
    * 11/4/2024
    
    * Complete Education Program Function
    
    * 15/4/2024

--- front/src/features/Admin/Class/AddClass/index.jsx
+++ front/src/features/Admin/Class/AddClass/index.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import "./index.scss";
 
-const UserList = () => {
+const AddClass = () => {
     return (<div></div>);
 }
 
-export default UserList;
+export default AddClass;
