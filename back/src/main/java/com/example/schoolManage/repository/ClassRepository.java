96e04eeb57c826a7a5a7969eb7affe44aed6f366
vunhan2492001 <vunhan2492001@gmail.com>
2024-04-17 13:57:36 +0700

    nothing update

--- back/src/main/java/com/example/schoolManage/repository/ClassRepository.java
+++ back/src/main/java/com/example/schoolManage/repository/ClassRepository.java
@@ -5,12 +5,14 @@ import com.example.schoolManage.repository.custom.ClassRepositoryCustom;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
-
 import java.util.List;
 import java.util.Optional;
+
 @Repository
 public interface ClassRepository extends MongoRepository<Classroom, String>, ClassRepositoryCustom {
     Optional<Classroom> findBySubject(String subject);
+
     List<Classroom> findAllByTeacher(String teacher);
+
     Optional<Classroom> findByName(String name);
 }
