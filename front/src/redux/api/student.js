f3bd40c648719962761aedf6c49b7255047bd62b
vunhan2492001 <vunhan2492001@gmail.com>
2024-04-18 03:42:30 +0700

    complete show Student Infor UI

---
+++ front/src/redux/api/student.js
@@ -0,0 +1,33 @@
+import { transport } from "../../config/http/transport";
+import Cookies from "js-cookie";
+
+const username = Cookies.get('username');
+const student = {
+    getInfo: () => {
+        return transport.get(
+            `/students/${username}`, JSON.stringify()
+        )
+    },
+    getAllClasses: () => {
+        return transport.get(
+            `/students/${username}/classes`, JSON.stringify()
+        )
+    },
+    updateStudent: (data) => {
+        return transport.put(
+            `/students/${username}`, JSON.stringify(data)
+        )
+    },
+    enrollClass: (data) => {
+        return transport.put(
+            `/students/${username}/enroll/${data}`, JSON.stringify()
+        )
+    },
+    unenrollClass: (data) => {
+        return transport.put(
+            `/students/${username}/unenroll/${data}`, JSON.stringify()
+        )
+    },
+}
+export default student;
+    console.log(username);
