99a898b5b8786d17abe92535e6a1cf7b2f035180
Hồ Nhật Trị <115467395+nhattribk22@users.noreply.github.com>
2024-04-19 14:12:22 +0700

    add teacher give scoore
    ok

--- back/src/main/java/com/example/schoolManage/service/TeacherService.java
+++ back/src/main/java/com/example/schoolManage/service/TeacherService.java
@@ -1,6 +1,7 @@
 package com.example.schoolManage.service;
 
 import com.example.schoolManage.model.course.Classroom;
+import com.example.schoolManage.model.user.Student;
 import com.example.schoolManage.model.user.Teacher;
 import com.example.schoolManage.repository.ClassRepository;
 import com.example.schoolManage.repository.UserRepository;
@@ -31,5 +32,20 @@ public class TeacherService {
         if(update.getEmail()!= null) teacher.get().setEmail(update.getEmail());
         if(update.getPhone()!= null) teacher.get().setPhone(update.getPhone());
         return Optional.of(userRepository.save(teacher.get()));
+    }
+    public void giveScore(String className, List<Double> scores) {
+        Optional<Classroom> classOptional = classRepository.findByName(className);
+        Classroom classroom = classOptional.get();
+        List<String> students = classroom.getStudents();
+        for (int i = 0; i < students.size(); i++) {
+            String studentUsername = students.get(i);
+            Double score = scores.get(i);
+            Optional<Student> studentOptional = userRepository.findStudentByUsername(studentUsername);
+            if (studentOptional.isPresent()) {
+                Student student = studentOptional.get();
+                student.getScores().put(classroom.getName(), score);
+                userRepository.save(student);
+            }
+        }
     }
 }
