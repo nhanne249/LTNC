3331a5f8a5e86f04effc12747880541ae82e99b3
vunhan2492001 <vunhan2492001@gmail.com>
2024-04-27 19:14:55 +0700

    fixed cors error

--- back/src/main/java/com/example/schoolManage/config/WebConfig.java
+++ back/src/main/java/com/example/schoolManage/config/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(@NonNull CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins("https://ltnc.vercel.app")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
