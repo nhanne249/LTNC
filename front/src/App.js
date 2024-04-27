dafcffc54ef789d9ed4a5f5679775e0a00a3f63d
vunhan2492001 <vunhan2492001@gmail.com>
2024-04-28 01:36:09 +0700

    fixed Route

--- front/src/App.js
+++ front/src/App.js
@@ -1,22 +1,25 @@
 import "./App.css";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
 import { useLocation, useNavigate } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { publicRouter, privateRouter } from "./config/routes";
-import {useCookies} from 'react-cookie';
 import Cookies from 'js-cookie';
-import { useEffect } from "react";
 function App() {
-  const [cookies] = useCookies(['isBrowserClose', 'role']);
-  var role=null;
-  if (Cookies.get('role')) {role = Cookies.get('role').toLowerCase();
+  const location = useLocation();
+  const pathname = location.pathname;
+  var role = null;
+  if (Cookies.get('role')) {
+    role = Cookies.get('role').toLowerCase();
   }
+  const pathParts = pathname.split("/").filter(Boolean);
+  var roleFromPath = pathParts[0];
   return (
     <>
       <Router>
         <Routes>
-          {role!=null || role != undefined ? (privateRouter.map((routers) => {
+          {role != null || role != undefined ? (privateRouter.map((routers,i) => {
+            if (roleFromPath == '/') return <Navigate to={`${role}`} key={i} />;
             return routers.map((route, index) => {
               return route.role == role ? (
                 <Route path={route.path} element={route.element} key={index}>
