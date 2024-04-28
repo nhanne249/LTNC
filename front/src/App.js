06f2010dca389d69211837417fb5042b7a0e7cb2
vunhan2492001 <vunhan2492001@gmail.com>
2024-04-28 21:10:09 +0700

    test

--- front/src/App.js
+++ front/src/App.js
@@ -1,59 +1,52 @@
 import "./App.css";
-import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes, Navigate, useNavigate } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { publicRouter, privateRouter } from "./config/routes";
 import Cookies from 'js-cookie';
 
 function App() {
-  const role = Cookies.get('role')?.toLowerCase(); // Optional chaining for nullish check
-  console.log(role);
+  const isLoggedIn = Cookies.get('userPresent') !== null && Cookies.get('userPresent') !== undefined;
+  const role = Cookies.get('role')?.toLowerCase();
 
   return (
     <>
       <Router>
         <Routes>
-          {role !== null && role !== undefined ? (
+          {isLoggedIn ? (
+            // Trường hợp đã login
+            //Lọc qua từng route đã liệt kê theo role
             privateRouter.map((routers) => (
+              //Lọc qua từng route ứng với role
               routers.map((route, index) => (
-                route.role === role ? (
-                  <Route path={route.path} element={route.element} key={index}>
+                //Xử lý trường hợp route đúng với role
+                //Khi Route đúng với role thì xem xét path mà người dùng đang truy cập có hợp lệ hay không nếu không thì sẽ tự động chuyển về path chính
+                route.role === role ? (()=> { window.location.pathname == role ?
+                  (<Route path={route.path} element={route.element} key={index}>
                     {route.index ? <Route index element={route.index} /> : null}
-                    {route.children ? (
-                      route.children.map(({ path, Component }, index) => (
-                        <Route
-                          path={path}
-                          element={<Component />}
-                          key={index}
-                        />
-                      ))
-                    ) : null}
-                  </Route>
-                ) : (() => { return <Navigate to={`${role}`} replace /> }
-                )
+                    {route.children?.map(({ path, Component }, index) => (
+                      <Route path={path} element={<Component />} key={index} />
+                    ))}
+                  </Route>): (<Route path="*" key={index} element={<Navigate to={`/${role}`} replace />} />)}
+                ) : (
+                  null)
               ))
             ))
           ) : (
+            // Trường hợp chưa login
             publicRouter.map((routers) => (
               routers.map((route, index) => (
                 <Route path={route.path} element={route.element} key={index}>
                   {route.index ? <Route index element={route.index} /> : null}
-                  {route.children ? (
-                    route.children.map(({ path, Component }, index) => (
-                      <Route
-                        path={path}
-                        element={<Component />}
-                        key={index}
-                      />
-                    ))
-                  ) : null}
+                  {route.children?.map(({ path, Component }, index) => (
+                      <Route path={path} element={<Component />} key={index} />
+                    ))}
                 </Route>
               ))
             ))
           )}
         </Routes>
       </Router>
-      {/* <Loading /> */}
       <ToastContainer />
     </>
   );
