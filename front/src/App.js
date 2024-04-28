a83b7f76736e9f692cd2fdaa1397b4b7c9fd76d1
vunhan2492001 <vunhan2492001@gmail.com>
2024-04-28 15:36:02 +0700

    update

--- front/src/App.js
+++ front/src/App.js
@@ -4,54 +4,51 @@ import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { publicRouter, privateRouter } from "./config/routes";
 import Cookies from 'js-cookie';
-function App() {
 
-  var role=null;
-  if (Cookies.get('role')) {role = Cookies.get('role').toLowerCase();
-  }
+function App() {
+  const role = Cookies.get('role')?.toLowerCase(); // Optional chaining for nullish check
+
   return (
     <>
       <Router>
         <Routes>
-          {role!=null || role != undefined ? (privateRouter.map((routers) => {
-            return routers.map((route, index) => {
-              return route.role == role ? (
-                <Route path={route.path} element={route.element} key={index}>
-                  {route.index ? <Route index element={route.index} /> : null}
-                  {route.children
-                    ? route.children.map(({ path, Component }, index) => {
-                      return (
-                        <Route
-                          path={path}
-                          element={<Component/> }
-                          key={index}
-                        />
-                      );
-                    })
-                    : null}
-                </Route>
-              ) : null;
-            });
-          })) : (publicRouter.map((routers) => {
-            return routers.map((route, index) => {
-              return (
-                <Route path={route.path} element={route.element} key={index}>
-                  {route.index ? <Route index element={route.index} /> : null}
-                  {route.children
-                    ? route.children.map(({ path, Component }, index) => {
-                      return (
+          {role !== null && role !== undefined ? (
+            privateRouter.map((routers) => (
+              routers.map((route, index) => (
+                route.role === role ? (
+                  <Route path={route.path} element={route.element} key={index}>
+                    {route.index ? <Route index element={route.index} /> : null}
+                    {route.children ? (
+                      route.children.map(({ path, Component }, index) => (
                         <Route
                           path={path}
                           element={<Component />}
                           key={index}
                         />
-                      );
-                    })
-                    : null}
+                      ))
+                    ) : null}
+                  </Route>
+                ) : null
+              ))
+            ))
+          ) : (
+            publicRouter.map((routers) => (
+              routers.map((route, index) => (
+                <Route path={route.path} element={route.element} key={index}>
+                  {route.index ? <Route index element={route.index} /> : null}
+                  {route.children ? (
+                    route.children.map(({ path, Component }, index) => (
+                      <Route
+                        path={route.path}
+                        element={<Component />}
+                        key={index}
+                      />
+                    ))
+                  ) : null}
                 </Route>
-              );
-            });
-          }))}
+              ))
+            ))
+          )}
         </Routes>
       </Router>
       {/* <Loading /> */}
