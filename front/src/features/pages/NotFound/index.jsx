6ec108847313629630181e3f0b2a7262385e0778
vunhan2492001 <vunhan2492001@gmail.com>
2024-04-29 23:40:29 +0700

    update 29/4/2024

--- front/src/features/pages/NotFound/index.jsx
+++ front/src/features/pages/NotFound/index.jsx
@@ -9,36 +9,8 @@ const NotFound = () => {
   const role = Cookies.get("role").toLowerCase();
   return (
     <div className="not-found-page">
-      <svg style={{ width: "400", height: "100" }}>
-        <text
-          style={{
-            fill: "white",
-            fillOpacity: "0.4",
-            fontSize: "80",
-            x: "200",
-            y: "70",
-            textAnchor: "middle",
-            stroke: "black",
-          }}
-        >
-          Not Found
-        </text>
-      </svg>
-      <svg style={{ width: "400", height: "100" }}>
-        <text
-          style={{
-            fill: "white",
-            fillOpacity: "0.4",
-            fontSize: "80",
-            x: "200",
-            y: "70",
-            textAnchor: "middle",
-            stroke: "black",
-          }}
-        >
-          404
-        </text>
-      </svg>
+      <div className="content">Not found</div>
+      <div className="content">404</div>
       <Button
         className="not-found-btn"
         onClick={() => navigate(`/${role}`, { replace: true })}
