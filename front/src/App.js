9cb2c08b8cb756b475a407b09f80fff76c13fcf7
vunhan2492001 <vunhan2492001@gmail.com>
2024-05-01 11:04:47 +0700

    -

--- front/src/App.js
+++ front/src/App.js
@@ -51,7 +51,7 @@ function App() {
               );
             });
           }))}
-          <Route path="*" element={<NotFound />} />
+          {/* <Route path="*" element={<NotFound />} /> */}
         </Routes>
       </Router>
       {/* <Loading /> */}
