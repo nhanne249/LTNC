c546519a33bc781f487fa752b1a139e5716e8c99
vunhan2492001 <vunhan2492001@gmail.com>
2024-05-01 09:32:31 +0700

    -

--- front/src/features/Teacher/Class/index.jsx
+++ front/src/features/Teacher/Class/index.jsx
@@ -54,35 +54,35 @@ const Class = () => {
   };
   const onInputScore = (value) => {
     console.log(value);
-    // dispatch(
-    //   giveScoreForStudentThunk({
-    //     username: usernameToSend,
-    //     score: value,
-    //     subject: subjectToSend,
-    //   })
-    // ).then((res) => {
-    //   console.log(res);
-    //   if (res?.error) {
-    //     toast.error("Tạo lớp học mới thất bại!", {
-    //       position: "top-right",
-    //       autoClose: 3000,
-    //       theme: "colored",
-    //     });
-    //   } else {
-    //     toast.success("Tạo lớp học mới thành công!", {
-    //       position: "top-right",
-    //       autoClose: 3000,
-    //       theme: "colored",
-    //     });
-    //     const dataSend = {
-    //       name: classNameOnShow,
-    //       page: 1,
-    //     };
-    //     dispatch(getClassThunk(dataSend)).then((res) => {
-    //       setStudentList(res?.payload);
-    //     });
-    //   }
-    // });
+    dispatch(
+      giveScoreForStudentThunk({
+        username: usernameToSend,
+        score: value,
+        subject: subjectToSend,
+      })
+    ).then((res) => {
+      console.log(res);
+      if (res?.error) {
+        toast.error("Tạo lớp học mới thất bại!", {
+          position: "top-right",
+          autoClose: 3000,
+          theme: "colored",
+        });
+      } else {
+        toast.success("Tạo lớp học mới thành công!", {
+          position: "top-right",
+          autoClose: 3000,
+          theme: "colored",
+        });
+        const dataSend = {
+          name: classNameOnShow,
+          page: 1,
+        };
+        dispatch(getClassThunk(dataSend)).then((res) => {
+          setStudentList(res?.payload);
+        });
+      }
+    });
   };
   const columns = [
     {
