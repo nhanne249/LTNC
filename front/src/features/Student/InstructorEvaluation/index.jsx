df31e2866f69827d7bb877b2ed6d8c8d6374dcda
vunhan2492001 <vunhan2492001@gmail.com>
2024-05-02 11:10:27 +0700

    2-5-2024

--- front/src/features/Student/InstructorEvaluation/index.jsx
+++ front/src/features/Student/InstructorEvaluation/index.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { Form, Button, Rate, Input, Flex } from "antd";
+import { Form, Button, Rate, Input, Flex, Menu } from "antd";
 import Cookies from "js-cookie";
 import { useDispatch } from "react-redux";
 import { getAllClassesThunk } from "../../../redux/action/student";
@@ -14,7 +14,11 @@ const InstructorEvaluation = () => {
   useEffect(() => {
     dispatch(getAllClassesThunk()).then((res) => {
       console.log(res);
-      setDataReceived(res?.payload);
+      setDataReceived([
+        res?.payload.map((data) => {
+          return { key: data.teacher, label: data.teacher };
+        }),
+      ]);
       setIsReceived(true);
     });
   }, [isReceived]);
@@ -22,28 +26,32 @@ const InstructorEvaluation = () => {
   //Gửi review
   const onFinish = (value) => {
     console.log(value);
+    console.log(dataReceived);
   };
 
   return (
-    <Flex vertical={true}>
-      <Flex vertical={true}>
-        <div>adsadsda</div>
+    <div className="evaluate-input">
+      <Flex vertical={false}>
+        <Menu></Menu>
+        <Flex vertical={true}>
+          <div>adsadsda</div>
+        </Flex>
+        <Form onFinish={onFinish} autoComplete="off" layout="vertical">
+          <div>{username}</div>
+          <Form.Item name="rating">
+            <Rate />
+          </Form.Item>
+          <Form.Item name="content">
+            <TextArea rows={4} placeholder="Đánh giá" />
+          </Form.Item>
+          <Form.Item>
+            <Button htmlType="submit" type="submit">
+              Gửi
+            </Button>
+          </Form.Item>
+        </Form>
       </Flex>
-      <Form onFinish={onFinish} autoComplete="off" layout="vertical">
-        <div>{username}</div>
-        <Form.Item name="rating">
-          <Rate />
-        </Form.Item>
-        <Form.Item name="content">
-          <TextArea rows={4} placeholder="Đánh giá" />
-        </Form.Item>
-        <Form.Item>
-          <Button htmlType="submit" type="submit">
-            Gửi
-          </Button>
-        </Form.Item>
-      </Form>
-    </Flex>
+    </div>
   );
 };
 
