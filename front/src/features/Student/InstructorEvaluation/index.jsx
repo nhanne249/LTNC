7ab24750b61f11958ae6ce080a1002b7f2ca8035
vunhan2492001 <vunhan2492001@gmail.com>
2024-05-02 12:26:54 +0700

    2-5-2024

--- front/src/features/Student/InstructorEvaluation/index.jsx
+++ front/src/features/Student/InstructorEvaluation/index.jsx
@@ -46,33 +46,36 @@ const InstructorEvaluation = () => {
 
   return (
     <div className="evaluate-input">
-      <Flex vertical={false}>
+      <Flex vertical={false} gap="small">
         <Menu
           theme="light"
           mode="vertical"
           items={dataReceived}
           onClick={(key) => onClickMenu(key)}
+          style={{ width: "250px" }}
         />
-        {teacherUsernameToShow ? (
-          <Flex vertical={true}>
-            <Form onFinish={onFinish} autoComplete="off" layout="vertical">
-              <div>{username}</div>
-              <Form.Item name="rating">
-                <Rate />
-              </Form.Item>
-              <Form.Item name="content">
-                <TextArea rows={4} placeholder="Đánh giá" />
-              </Form.Item>
-              <Form.Item>
-                <Button htmlType="submit" type="submit">
-                  Gửi
-                </Button>
-              </Form.Item>
-            </Form>
-          </Flex>
-        ) : (
-          <div>Bảng đánh giá</div>
-        )}
+        <div className="review-container">
+          {teacherUsernameToShow ? (
+            <Flex vertical={true}>
+              <Form onFinish={onFinish} autoComplete="off" layout="vertical">
+                <div>{username}</div>
+                <Form.Item name="rating">
+                  <Rate />
+                </Form.Item>
+                <Form.Item name="content">
+                  <TextArea rows={4} placeholder="Đánh giá" />
+                </Form.Item>
+                <Form.Item>
+                  <Button htmlType="submit" type="submit">
+                    Gửi
+                  </Button>
+                </Form.Item>
+              </Form>
+            </Flex>
+          ) : (
+            <div>Bảng đánh giá</div>
+          )}
+        </div>
       </Flex>
     </div>
   );
