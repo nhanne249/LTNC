14c47188067e6e323aca6e41ddddc47c5dca9f65
blueonline07 <ldkhang1201@gmail.com>
2024-05-05 17:13:51 +0700

    clean

--- back/src/main/java/com/example/schoolManage/repository/ResourceRepository.java
+++ back/src/main/java/com/example/schoolManage/repository/ResourceRepository.java
@@ -3,10 +3,11 @@ package com.example.schoolManage.repository;
 import com.example.schoolManage.model.files.Resource;
 import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
-
+@Repository
 public interface ResourceRepository extends MongoRepository<Resource, ObjectId> {
     List<Resource> findAllByClassroom(String classroom);
     void deleteByNameAndClassroom(String name, String classname);
