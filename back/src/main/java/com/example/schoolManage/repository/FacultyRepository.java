efc9514ab11040d713d374b152e44a1a6f9c8ca6
blueonline07 <ldkhang1201@gmail.com>
2024-05-05 17:09:31 +0700

    delete subject/ fac is forbidden when subject is active

--- back/src/main/java/com/example/schoolManage/repository/FacultyRepository.java
+++ back/src/main/java/com/example/schoolManage/repository/FacultyRepository.java
@@ -3,9 +3,11 @@ package com.example.schoolManage.repository;
 import com.example.schoolManage.model.shared.Faculty;
 import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface FacultyRepository extends MongoRepository<Faculty, ObjectId> {
     Optional<Faculty> findByName(String name);
 
